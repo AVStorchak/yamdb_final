700caef704e5258931f6c590ebec95f9026e2607
Андрей Сторчак <stor-andrej@yandex.ru>
2021-01-13 21:44:07 +0300

    Completed project

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -133,8 +133,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
-
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
